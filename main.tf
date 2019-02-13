274956f5c6d1c8c67316d4feb189d784a8d0c09d
Serhii Tretiak <seradm28@gmail.com>
2019-02-13 16:42:25 +0200

    my first commit

---
+++ main.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "My-VPC"
+    Owner = "Serg"
+  }
+}
+
+
+/*
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+*/
+
+#Public Subnet
+
+resource "aws_subnet" "us-east-2a-public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+#  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "My-Public-Subnet-A"
+    Owner = "Serg"
+  }
+}
+
+resource "aws_route_table" "us-east-2a-public" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags {
+        Name = "My-Route Table Public"
+        Owner = "Serg"
+    }
+}
+
+resource "aws_route_table_association" "us-east-2a-public" {
+    subnet_id = "${aws_subnet.us-east-2a-public.id}"
+    route_table_id = "${aws_route_table.us-east-2a-public.id}"
+}
+
+resource "aws_subnet" "us-east-2b-privat" {
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${var.private_subnet_cidr}"
+#  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "My-Privat-Subnet-A"
+    Owner = "Serg"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "My-Internet-Gateway"
+    Owner = "Serg"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,32 @@
+#variable "aws_key_path" {}
+#variable "aws_key_name" {}
+
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "us-east-2"
+}
+
+#variable "amis" {
+#    description = "AMIs by region"
+#    default = {
+#        us-east-2 = "ami-02e1499f8253f416f" # ubuntu 18.04 LTS
+#    }
+#}
+
+
+
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.10.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.20.0/24"
+}
